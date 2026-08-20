af159699bc37a850f73c03544a04a9872aecc093
notteelove <notteelove0099@gmail.com>
2026-08-21 00:36:06 +0700

    chore: add products data file

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const products = [
-  { code: "P001", name: "ดินสอ", quantity: 10 }
-];
+const fs = require("fs");
 
-function addProduct(code, name, quantity) {
-  const duplicate = products.some(product => product.code === code);
-
-  if (duplicate) {
-    console.log("สินค้านี้มีอยู่แล้ว");
-    return;
-  }
-
-  products.push({ code, name, quantity });
-  console.log("เพิ่มสินค้าใหม่สำเร็จ");
+function saveProducts(products) {
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
 }
