cfaeb79392029d0ea8dce39ec0efefdaaa2cc2ca
notteelove <notteelove0099@gmail.com>
2026-08-21 00:51:41 +0700

    feat: save product data to file

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const fs = require("fs");
 
-function saveProducts(products) {
+let products = [];
+
+function saveProducts() {
   fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
 }
+
+function addProduct(code, name, quantity) {
+  const duplicate = products.some(product => product.code === code);
+
+  if (duplicate) {
+    console.log("สินค้านี้มีอยู่แล้ว");
+    return;
+  }
+
+  products.push({ code, name, quantity });
+  saveProducts();
+  console.log("เพิ่มสินค้าใหม่สำเร็จ");
+}
+
+addProduct("P001", "ดินสอ", 10);
